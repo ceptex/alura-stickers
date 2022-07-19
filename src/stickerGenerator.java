9c315a452756dc4298e9195bcb7b19893d61787a
Caio Teixeira <cepteixeira@gmail.com>
2022-07-19 17:45:14 -0300

    created a Sticker Generator based on the movie poster -- need to fix output file

---
+++ src/stickerGenerator.java
@@ -0,0 +1,40 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.InputStream;
+import java.net.URL;
+
+import javax.imageio.ImageIO;
+
+public class stickerGenerator {
+
+    public void cria(InputStream inputStream, String nomeArquivo) throws Exception {
+        
+        // leitura da imagem
+        //InputStream inputStream = new URL("https://imersao-java-apis.s3.amazonaws.com/TopMovies_1.jpg").openStream();
+        BufferedImage imagemOriginal = ImageIO.read(inputStream);
+
+        // cria nova imagem em memória com transparência e tamanho novo
+        int largura = imagemOriginal.getWidth();
+        int altura = imagemOriginal.getHeight();
+        int novaAltura = altura + 200;
+        BufferedImage novaImagem = new BufferedImage(largura, novaAltura, BufferedImage.TRANSLUCENT);
+
+        // copiar a imagem original para nova imagem (em memória)
+        Graphics2D graphics = (Graphics2D) novaImagem.getGraphics();
+        graphics.drawImage(imagemOriginal, null, 0, 0);
+
+        // configurar fonte
+        var fonte = new Font(Font.SANS_SERIF, Font.BOLD, 64);
+        graphics.setColor(Color.YELLOW);
+        graphics.setFont(fonte);
+
+        // escrever uma frase na nova imagem
+        graphics.drawString("TOPZERA!?", largura - 480, novaAltura - 80);
+
+        // escrever a nova imagem no arquivo
+        ImageIO.write(novaImagem, "png", new File("/saida/" + nomeArquivo));
+    }
+}
